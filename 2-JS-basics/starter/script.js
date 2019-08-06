d25ae538ef05e3fae9862458bda4efb196dc8c07
Ryan Carr <rycarr@gmail.com>
2019-08-05 20:10:05 -0500

    Continued on with the course
    Covered the basics of operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -22,3 +22,69 @@ var _3years = 3;
 var johnMark = 'John and MArk';
 var if = 23;
 */
+
+/*******************************
+ * Variable mutation and type coercion
+ */
+
+ /*
+ var firstName = 'John';
+ var age = 28;
+
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age +
+ ' year old ' + job + '. Is he married? ' + isMarried);
+
+ age = 'twenty eight';
+ job = 'driver';
+
+ alert(firstName + ' is a ' + age + ' year old ' + 
+ job + '. Is he married? ' + isMarried);
+
+ var lastName = prompt('What is his last name');
+
+ console.log(firstName + ' ' + lastName);
+ */
+
+/*******************************
+ * Basic Operators
+ */
+/*
+var now, yearJohn, yearMark, ageJohn, ageMark;
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10); // Doesn't use floor division
+
+// Logical operators
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+
+/*******************************
+ * Operator Precedence
+ */
