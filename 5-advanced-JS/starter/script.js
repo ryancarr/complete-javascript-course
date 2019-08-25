1b82e2b9e60c2c427e218def308017f2a3214a41
Ryan Carr <rycarr@gmail.com>
2019-08-25 18:49:46 -0500

    Learned about Function Constructors and prototypes

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,27 @@
+// Function Constructor
+var Person = function(name, yearOfBirth, job)
+{
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() 
+{
+    return 2019 - this.yearOfBirth;
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+console.log(john.name + ' was born in ' + john.yearOfBirth + ' and currently is a ' + john.job + '.');
+console.log(john.name + ' is ' + john.calculateAge() + ' years old.');
+console.log(jane.name + ' is ' + jane.calculateAge() + ' years old.');
+console.log(mark.name + ' is ' + mark.calculateAge() + ' years old.');
+
+console.log(john.name + ' ' + john.lastName);
+console.log(jane.name + ' ' + jane.lastName);
+console.log(mark.name + ' ' + mark.lastName);
